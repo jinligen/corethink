55fa746ff8c7487578b6b5a0346bddee1f10f967
hzj <1163828436@qq.com>
2017-09-12 14:11:40 +0800

    备份还原

--- BackUp/sd/2017-09-11/oc_admin_session.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS `oc_admin_session`;
-CREATE TABLE `oc_admin_session` (
-  `session_id` varchar(255) NOT NULL,
-  `session_expire` int(11) NOT NULL,
-  `session_data` blob,
-  UNIQUE KEY `session_id` (`session_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='session存储表';
-
