cbcc265d10a462fe52a020b3af6d02f233b63f1b
zhaohuanshi <1163828436@qq.com>
2017-10-01 00:21:37 +0800

    对账单

--- Application/Admin/Controller/BaoBiaoController.class.php
+++ Application/Admin/Controller/BaoBiaoController.class.php
@@ -493,7 +493,164 @@ class BaoBiaoController extends CommonController
     public function supplierBill()
     {
 
-        $this->display();
+        if (IS_POST) {
+            $field1 = '
+                customer_id                     as customer_id,
+                customer_name                   as customer_name,
+                entry_order_type_name           as order_type_name,
+                entry_order_date                as order_date,
+                entry_order_id                  as order_id,  
+                entry_order_remark              as order_remark, 
+                entry_order_account_payable     as order_should_payment, 
+                entry_order_actual_payment      as order_actual_payment,
+                entry_order_including_tax_price as order_deposit_price,
+                \'\'                            as end_should_payment,
+                department                      as department, 
+                nickname                        as nickname
+            ';
+
+            $field2 = '
+                customer_id                         as customer_id,
+                customer_name                       as customer_name,
+                payment_order_type_name             as order_type_name,
+                payment_order_date                  as order_date,
+                payment_order_id                    as order_id,  
+                payment_order_remark                as order_remark, 
+                payment_order_should_payment_price  as order_should_payment, 
+                payment_order_actual_payment        as order_actual_payment,
+                payment_order_deposit_after_price   as order_deposit_price,
+                \'\'                                as end_should_payment,
+                department                          as department, 
+                nickname                            as nickname
+            ';
+
+
+
+            if($_POST['start_date']){
+                $where1 = "entry_order_date >='".$_POST['start_date']."' and entry_order_date <= '9999-99-99'";
+                $where2 = "payment_order_date >='".$_POST['start_date']."' and payment_order_date <= '9999-99-99'";
+            }
+            if($_POST['end_date']){
+                $where1 = "entry_order_date >='0000-00-00' and entry_order_date <= '".$_POST['end_date']."'";
+                $where2 = "payment_order_date >='0000-00-00' and payment_order_date <= '".$_POST['end_date']."'";
+            }
+            if($_POST['start_date']&&$_POST['end_date']){
+                $where1 = "entry_order_date >='".$_POST['start_date']."' and entry_order_date <= '".$_POST['end_date']."'";
+                $where2 = "payment_order_date >='".$_POST['start_date']."' and payment_order_date <= '".$_POST['end_date']."'";
+            }
+
+            $where1 .= " and entry_order_type_name like '%采购%' and customer_id like '%".$_POST['customer_id']."%'";
+            $where2 .= " and customer_id like '%".$_POST['customer_id']."%'";
+
+
+
+
+            $list = D('storehouse_entry_order')
+                ->field($field1)
+                ->where($where1)
+                ->union(array('field'=>$field2,'table'=>'oc_storehouse_payment_order','order'=>'order_date','where'=>$where2))
+//                ->fetchSql(true)
+                ->select() ;
+//            echo $list;
+//            exit;
+
+            $sql = " SELECT customer_id,customer_name,SUM(order_should_payment)AS order_should_payment,SUM(order_actual_payment) AS order_actual_payment ".
+                    ",SUM(order_should_payment)-SUM(order_actual_payment) AS end_should_payment".
+                    " FROM (".
+                    "  SELECT customer_id                  AS customer_id,".
+                    "         customer_name                AS customer_name,".
+                    "         entry_order_account_payable  AS order_should_payment,".
+                    "         entry_order_actual_payment   AS order_actual_payment  ".
+                    "    FROM `oc_storehouse_entry_order`  ".
+                    "    WHERE  ".$where1.
+                    "    UNION ".
+                    "    SELECT customer_id                        AS customer_id,".
+                    "           customer_name                      AS customer_name,".
+                    "           payment_order_should_payment_price AS order_should_payment,".
+                    "           payment_order_actual_payment       AS order_actual_payment ".
+                    "    FROM `oc_storehouse_payment_order`  ".
+                    "    WHERE ".$where2.
+                    " ) AS temp ".
+                    " GROUP BY customer_id";
+            $Model = new \Think\Model(); // 实例化一个model对象 没有对应任何数据表
+            $customer_list = $Model->query($sql);
+            $count = 0;
+            $t = '';
+            if ($list && count($list) > 1) {
+                for ($i = 0, $len = count($list); $i < count($list); $i++) {
+                    if ($i==0 ) {
+                        array_splice($list,$i,0,array(array(
+                            customer_id => $list[$i]['customer_id'],
+                            customer_name => $list[$i]['customer_name'],
+                            order_type_name => $list[$i]['customer_name'],
+                            order_date => "",
+                            order_id => "",
+                            order_remark => "",
+                            order_should_payment => "",
+                            order_actual_payment => "",
+                            order_deposit_price => "",
+                            end_should_payment =>$customer_list[$count]['end_should_payment'],
+                            department => "",
+                            nickname => "",
+                        )));
+
+                    }else if($list[$i - 1] && $list[$i]['customer_id'] != $list[$i - 1]['customer_id']){
+                        array_splice($list,$i,0,array(array(
+                            customer_id => $list[$i]['customer_id'],
+                            customer_name => $list[$i]['customer_name'],
+                            order_type_name => $list[$i]['customer_name'],
+                            order_date => "",
+                            order_id => "",
+                            order_remark => "",
+                            order_should_payment => "",
+                            order_actual_payment => "",
+                            order_deposit_price => "",
+                            end_should_payment =>$customer_list[$count++]['end_should_payment'],
+                            department => "",
+                            nickname => "",
+                        )));
+                        $i++;
+
+                        $list[$i]['end_should_payment'] =$list[$i-1]['end_should_payment'] + $list[$i]['order_should_payment'] - $list[$i]['order_actual_payment'] ;
+                        array_splice($list,$i,0,array(array(
+                            customer_id => $list[$i]['customer_id'],
+                            customer_name => $list[$i]['customer_name'],
+                            order_type_name => $list[$i]['customer_name'],
+                            order_date => "",
+                            order_id => "",
+                            order_remark => "",
+                            order_should_payment => "",
+                            order_actual_payment => "",
+                            order_deposit_price => "",
+                            end_should_payment =>$customer_list[$count]['end_should_payment'],
+                            department => "",
+                            nickname => "",
+                        )));
+                        $i++;
+
+                    }else{
+                        $list[$i]['end_should_payment'] =$list[$i-1]['end_should_payment'] + $list[$i]['order_should_payment'] - $list[$i]['order_actual_payment'] ;
+
+                    }
+
+
+                }
+            }
+
+            if(count($list)>0){
+                $_list  = json_encode($list,JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+                $ret = json_encode( '{error:"0000",msg:"查询成功！",list:'.$_list.'}',JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+
+            }else{
+                $ret = json_encode('{error:"0001",msg:"未查到任何数据！"}');
+
+            }
+
+            echo $ret;
+            exit;
+        }else{
+            $this->display();
+        }
 
     }
 
