80f0e0d5c194695d57eb8135f32f24cafc581da0
zhaohuanshi <1163828436@qq.com>
2017-10-08 22:23:21 +0800

    登录

--- Application/Admin/Model/UserModel.class.php
+++ Application/Admin/Model/UserModel.class.php
@@ -119,7 +119,9 @@ class UserModel extends Model
             $this->error = '用户不存在或被禁用！';
         } else {
             if (user_md5($password) !== $user_info['password']) {
-                $this->error = '密码错误！'.user_md5($password).'!=='.$user_info['password'];
+//                $this->error = '密码错误！'.user_md5($password).'!=='.$user_info['password'];
+                $this->error = '密码错误！';
+
             } else {
                 return $user_info;
             }
