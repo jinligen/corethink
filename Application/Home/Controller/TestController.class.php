c023d8d304411d4697bb1f6427a36d02c53323ae
zhaohuanshi <1163828436@qq.com>
2017-10-10 21:48:22 +0800

    123123

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -22,7 +22,38 @@ class TestController extends HomeController
      */
     public function index()
     {
-        echo 'ok';
+        $url='https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wx538077f2d77d0c5b&corpsecret=bkO2Dv9MFkwT0FwxB_LIXm5EzWinItdp_lfe9NkM4JuB5RBXdlo4TE5S4YIz3rmi';
+        $ret = $this->httpsGet($url);
+        if($ret['access_token']){
+            $ret1 = $this->httpsGet('https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token='.$ret['access_token']);
+            echo $ret1['ticket'];
+        }else{
+        }
+
     }
 
+    public function httpsGet($url){
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);// https请求不验证证书和hosts
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        $res=json_decode(curl_exec($curl),true);
+        curl_close($curl);
+        return $res;
+    }
+    public function httpsPost($url,$post_data){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // post数据
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // https请求 不验证证书和hosts
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        // post的变量
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        $output=json_decode(curl_exec($ch),true);
+        curl_close($ch);
+        return $output;
+    }
 }
